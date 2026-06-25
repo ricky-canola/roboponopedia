2151e327198c43611622e8797d302405c1b5b62a
Richard Knoll <okayitsrichard@gmail.com>
2026-06-24 18:57:01 -0700

    add base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
